98723381238b16691ec95d42b522dfa9f141b856
yuanyating3 <yuanyating3@jd.com>
2024-09-13 16:36:02 +0800

    init: setup dumi demo

---
+++ .umirc.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'Learn React Common Scenarios',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  base: 'learn_react_common_scenarios',
+  outputPath: 'learn_react_common_scenarios',
+  mode: 'site',
+  // more config: https://d.umijs.org/config
+});
