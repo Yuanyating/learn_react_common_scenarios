d85f16b0b3bc370c12a00ad52d6b61e922ef409c
Yuanyating <734699649@qq.com>
2024-09-22 23:27:08 +0800

    fix: config

--- .umirc.ts
+++ .umirc.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'learn_react_common_scenarios'; // 项目名
+
 export default defineConfig({
-  title: 'Learn React Common Scenarios',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  base: 'learn_react_common_scenarios',
-  outputPath: 'learn_react_common_scenarios',
+  title: 'learn_react_common_scenarios',
   mode: 'site',
+  devServer: {
+    port: 1998 // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  
   // more config: https://d.umijs.org/config
 });
